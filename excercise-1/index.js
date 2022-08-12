f10b4d320747ba27bb541354179157cb2c8b70ba
Quadatz <quadath@gmail.com>
2022-08-12 10:43:47 +0300

    task 1

---
+++ excercise-1/index.js
@@ -0,0 +1,19 @@
+ 
+const company1 = { name: 'Hexlet', state: 'moderating', website: 'https://hexlet.io' };
+const company2 = { name: 'CodeBasics', state: 'published', website: 'https://code-basics.com' };
+is(company1, company2); // false
+ 
+const company3 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+const company4 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+is(company3, company4); // true
+
+function is(first, second) {
+    let answer = true;
+    for (let i = 0; i < Object.keys(first).length; i++) {
+        keyName = Object.keys(first)[i];
+        if (first[keyName] != second[keyName]) {
+            answer = false;
+        }
+    }
+    console.log(answer);
+}
